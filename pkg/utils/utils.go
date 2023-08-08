1b9ac2c968efbbc27f99d8db1ccc7df4da28aa8f
begenov <begenovorazali@icloud.com>
2023-08-08 16:06:17 +0600

    update

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,1 +1,26 @@
 package utils
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
+const (
+	iinRegex      = `(\d{2}(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01]))[0-9]{6}`
+	alphabetOrInt = "abcdefghijklmnopqrstuvwxyz0123456789"
+)
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabetOrInt)
+	for i := 0; i < n; i++ {
+		c := alphabetOrInt[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+	return sb.String()
+}
+
+func RandomEmail() string {
+	return fmt.Sprintf("%s@gmail.com", RandomString(6))
+}
